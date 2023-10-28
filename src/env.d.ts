4345e0313a7e5820ec54a3b95e9a493d5c1297ff
Jaen Figueroa <jaenfigueroa03042000@gmail.com>
2023-10-28 10:40:54 -0500

    feat: typed env

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly DB_PASSWORD: string
+  readonly PUBLIC_POKEAPI: string
+  // más variables de entorno...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
