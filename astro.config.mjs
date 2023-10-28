299363e2f93fa9839a5f5606a861e36945119a73
Jaen Figueroa <jaenfigueroa03042000@gmail.com>
2023-10-28 11:56:59 -0500

    chore: configure astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,23 @@
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig()
+export default defineConfig({
+  // vite: {
+  //   build: {
+  //     rollupOptions: {
+  //       // Personalización de nombres de archivos compilados
+  //       output: {
+  //         entryFileNames: 'entry.[hash].mjs',
+  //         chunkFileNames: 'chunks/chunk.[hash].mjs',
+  //         assetFileNames: 'assets/asset.[hash][extname]',
+  //       },
+  //     },
+  //   },
+  // },
+  // // REDIRECCIONAAR URLS
+  // redirects: {
+  //   '/old': '/new',
+  // },
+  /* MINIFICAR LOS ARCHIVOS */
+  // compressHTML: false,
+})
